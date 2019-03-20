f2a6704a3a5f0409a1c5fdedb4fa0aa04af2cc84
root <969114624@qq.com>
2019-03-20 20:07:29 +0800

    sts first to git

---
+++ Bubble Sort/src/yyz/Sort/BubbleSort.java
@@ -0,0 +1,21 @@
+package yyz.Sort;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+public class BubbleSort {
+	public static void time() {
+		LocalDateTime localDateTime=LocalDateTime.now();
+		System.out.println("now the time is:"+localDateTime);
+		LocalDate localDate=localDateTime.toLocalDate();
+		Month month=localDateTime.getMonth();
+		int day=localDateTime.getDayOfMonth();
+		int second=localDateTime.getSecond();
+		System.out.println("月:"+month+"日"+day+"秒"+second);
+	}
+
+	public static void main(String args[]){
+		time();
+	}
+}
